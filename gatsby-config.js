a25ddb78376c0c0159a4c597ea12f1bf21fbd770
phuonggg <tieuphuong.ftu@gmail.com>
2020-05-13 11:58:06 +0900

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,36 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  // plugins: [`gatsby-mdx`],
+
+  plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `src/posts`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        gatsbyRemarkPlugins: [
+          {
+            resolve: "gatsby-remark-images",
+          },
+        ],
+        defaultLayouts: {
+          // posts: require.resolve("./src/templates/post-template.js"),
+          // default: require.resolve("./src/components/layout.js"),
+        },
+      },
+    },
+  ],
+}
